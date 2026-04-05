7fb4e1231ee1a64da4d0bdb69c679f2de6966049
FineDev7 <howareyou1771@gmail.com>
2026-04-05 13:55:21 +0530

    Add next.config.js with Next.js 14 settings.

---
+++ next.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Other Next.js configuration options can be added here
+};
+
+module.exports = nextConfig;
